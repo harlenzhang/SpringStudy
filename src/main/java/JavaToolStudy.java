9072dbc247f776ce5289ff3d3c67b776fd6bc4ee
harlenzhang <hanwen.zhang@tqmall.com>
2016-10-19 11:49:43 +0800

    JVM tool study

---
+++ src/main/java/JavaToolStudy.java
@@ -0,0 +1,11 @@
+/**
+ * Created by harlenzhang on 16/10/17.
+ */
+public class JavaToolStudy {
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("hello");
+            Object o = new Object();
+        }
+    }
+}
